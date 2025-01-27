26ca97ab07f274f48b38bcb8c773e366ce1738d6
Essyuge <ugeesther@gmail.com>
2025-01-27 11:11:01 +0300

    java based configuration

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,29 @@
 package org.example;
-import org.springframework.context.ApplicationContext;           // For ApplicationContext
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.example.config.AppConfig;
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-        Alien obj = (Alien) context.getBean("alien");
-        obj.code();
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+      Desktop dk = context.getBean( Desktop.class);
+         dk.compile();
+
+
+        Desktop dk1 = context.getBean( Desktop.class);
+        dk.compile();
+
+
+
+
+
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+//        Alien obj = (Alien) context.getBean("alien");
+//        obj.code();
 
     }
 }
